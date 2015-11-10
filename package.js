2f6136516a3aecbdb4110f2e42944776b9652a51
Francis Brunelle <frabrunelle@gmail.com>
2015-11-10 12:49:11 -0500

    set localMediaInterval to null when clearing interval

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name:'hitchcott:qr-scanner',
-  version:'1.0.2',
+  version:'1.0.3',
   summary: 'A QR Code Scanner (using jsqrcode)',
   git: "https://github.com/hitchcott/meteor-qr-code-scanner.git"
 });
